977b8e4fe2a7f654c3a0e433e1c4a88c0105fa73
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-13 15:26:46 -0400

    protein translation iteration 2

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,11 +1,10 @@
 def proteins(strand):
-    sequence = [strand[i:i+3] for i in range (0, len(strand), 3)]
-    dict = {'AUG':'Methionine', 'UUU':'Phenylalanine', 'UUC':'Phenylalanine', 'UUA':'Leucine','UUG':'Leucine', 'UCU':'Serine','UCC':'Serine','UCA':'Serine','UCG':'Serine', 'UAU':'Tyrosine','UAC':'Tyrosine','UGU':'Cysteine', 'UGC':'Cysteine', 'UGG':'Tryptophan'}
-    Stop = {'UAA', 'UAG', 'UGA'}
+    codons = {'AUG' : 'Methionine', 'UUU' : 'Phenylalanine', 'UUC' : 'Phenylalanine', 'UUA' : 'Leucine','UUG' : 'Leucine', 'UCU' : 'Serine','UCC' : 'Serine','UCA' : 'Serine','UCG' : 'Serine', 'UAU' : 'Tyrosine','UAC' : 'Tyrosine','UGU' : 'Cysteine', 'UGC' : 'Cysteine', 'UGG' : 'Tryptophan'}
+    stop = {'UAA', 'UAG', 'UGA'}
     protein = []
-    for e in sequence: 
-        if e not in Stop:
-           protein.append(dict[e])
+    for e in range(0, len(strand), 3): 
+        if not strand[e:e+3] in stop:
+           protein.append(codons[strand[e:e+3]])
         else:
            break
     return protein
