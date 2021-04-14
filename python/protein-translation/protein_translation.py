ab981a5d32018b8315e3ba58dc3463e43a1f9e86
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-13 23:18:54 -0400

    Update protein_translation.py

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,12 +1,20 @@
+#codons dictionary indicates the matching protein for the codon
+codons = {'AUG' : 'Methionine', 'UUU' : 'Phenylalanine',
+          'UUC' : 'Phenylalanine', 'UUA' : 'Leucine' ,
+          'UUG' : 'Leucine', 'UCU' : 'Serine',
+          'UCC' : 'Serine','UCA' : 'Serine', 
+          'UCG' : 'Serine', 'UAU' : 'Tyrosine', 
+          'UAC' : 'Tyrosine', 'UGU' : 'Cysteine',
+          'UGC' : 'Cysteine', 'UGG' : 'Tryptophan'}
 def proteins(strand):
-    codons = {'AUG' : 'Methionine', 'UUU' : 'Phenylalanine', 'UUC' : 'Phenylalanine', 'UUA' : 'Leucine','UUG' : 'Leucine', 'UCU' : 'Serine','UCC' : 'Serine','UCA' : 'Serine','UCG' : 'Serine', 'UAU' : 'Tyrosine','UAC' : 'Tyrosine','UGU' : 'Cysteine', 'UGC' : 'Cysteine', 'UGG' : 'Tryptophan'}
+    #stop set contains the STOP codon segments
     stop = {'UAA', 'UAG', 'UGA'}
     protein = []
     for e in range(0, len(strand), 3): 
-        if not strand[e:e+3] in stop:
-           protein.append(codons[strand[e:e+3]])
-        else:
+        if strand[e:e+3] in stop:
            break
+        #If the codon is not a stopping segement the it is added to the protein
+        protein.append(codons[strand[e:e+3]])
     return protein
 
         
