00f36c921ea9cfd3f809a34b716dbffe02f8f07d
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-17 10:32:22 -0400

    Update armstrong_numbers.py iteration 2

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,17 +1,20 @@
 import math
 def is_armstrong_number(number):
+ 
+    digits = [i for i in str(number)]
+    sum_digits = sum(math.pow(int(e), len(digits)) for e in digits)
+    return sum_digits == number
+
+    # simply do len(digits) doesn't recompute the value so better SOLVED
     """
     Redundancy: simply do str(number) to obtain a list. You call str(number) multiple times
     instead of once. This looks like:
         digits = [i for i in str(number)]
+    SOLVED
     """
-    digits = [str(number)[i] for i in range (0, len(str(number)))]
-    sum = 0
     """
     Optimal but in python, list comprehensions are usually faster when possible. This looks like
     the single call:
-        sum = sum(math.pow(int(e)) for e in digits)
+        sum_digits = sum(math.pow(int(e), len(digits)) for e in digits)
+    SOLVED
     """
-    for e in digits:
-        sum += math.pow(int(e), len(str(number))) # simply do len(digits) doesn't recompute the value so better
-    return sum == number
