71d8b718f096108edf25a8af1f57087729c6576c
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-20 11:33:49 -0400

    Update high_scores.py

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,19 +1,12 @@
 def latest(scores):
-    latest = scores[len(scores)-1] 
-    return latest
-
+    return scores[-1] 
 
 def personal_best(scores):
-    Max = max(scores)
-    return Max
+    return max(scores)
 
 def personal_top_three(scores):
-    top = []
     rg = 3
     if len(scores) < 3:
        rg = len(scores)
-    for _ in range(rg):
-        maxval = personal_best(scores)
-        top.append(maxval)
-        scores.remove(maxval)
-    return top
+    ranking = sorted(scores, reverse = True)
+    return [ranking[i] for i in range(rg)]
