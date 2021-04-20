1f3b27646fafe9f121ba41ca9962648f95a991d9
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-19 20:55:16 -0400

    Update high_scores.py

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,32 +1,19 @@
 def latest(scores):
-	latest = scores.pop() 
-	return latest
+    latest = scores[len(scores)-1] 
+    return latest
 
 
 def personal_best(scores):
-	Max = max(scores)
-	return Max
+    Max = max(scores)
+    return Max
 
 def personal_top_three(scores):
-        """
-        You can add right away a check to see if there are 3 scores at
-        least. If not, you can't return the top 3 scores:
-            if len(scores) < 3:
-                raise Exception("There is not a sufficient number of scores")
-        """
-	top = [max(scores)] # try to save max(scores) as variable
-        scores.remove(max(scores)) # you can reuse the variable in this line
-        """
-        Instead of doing multiple if then conditions, use a
-        loop. A for loop could be good:
-            for i in range(3): # can even use no variable by doing for _ in range(3)
-                maxval = personal_best(scores)
-                top.append(maxval)
-                scores.remove(maxval)
-        """
-	if len(scores)>0:
-	   top.append(max(scores))
-	   scores.remove(max(scores))
-	if len(scores)>0:
-	   top.append(max(scores))
-	return top
+    top = []
+    rg = 3
+    if len(scores) < 3:
+       rg = len(scores)
+    for _ in range(rg):
+        maxval = personal_best(scores)
+        top.append(maxval)
+        scores.remove(maxval)
+    return top
