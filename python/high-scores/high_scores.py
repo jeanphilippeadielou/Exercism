f105b2189182d99ca4b8544cc0c669e618438c2c
Jean-Philippe Adiélou <35672381+jeanphilippeadielou@users.noreply.github.com>
2021-04-20 12:21:27 -0400

    Approved high_scores.py

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -5,8 +5,5 @@ def personal_best(scores):
     return max(scores)
 
 def personal_top_three(scores):
-    rg = 3
-    if len(scores) < 3:
-       rg = len(scores)
     ranking = sorted(scores, reverse = True)
-    return [ranking[i] for i in range(rg)]
+    return ranking[:3]
