32def8d8aea15a1d7d840a23fee943e4d148546e
Jean-Philippe Adielou <jean-philippe.adielou@polymtl.ca>
2023-04-10 10:02:52 -0400

    rotational cipher solved

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,17 @@
 def rotate(text, key):
-    pass
+    plain = "abcdefghijklmnopqrstuvwxyz"
+    new_text = [] 
+    for x in text:
+        if 97 <= ord(x) < 123:
+            if ord(x) + key > 122:
+                new_text.append(chr(ord(x) + key - 26))
+            else:
+                new_text.append(chr(ord(x) + key))
+        if 65 <= ord(x) <= 90:
+            if ord(x) + key >= 90:
+                new_text.append(chr(ord(x) + key - 26))
+            else:
+                new_text.append(chr(ord(x) + key))
+        if not x.isalpha():
+            new_text.append(x)
+    return "".join(new_text)
