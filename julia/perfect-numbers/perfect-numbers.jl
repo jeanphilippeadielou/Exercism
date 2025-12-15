cc7bebab34d7c6647ac04d80c6df5ffd2d11e348
[Jean-Philippe Adielou] <[jeanphilippeadielou@gmail.com]>
2025-12-15 04:30:44 -0500

    2nd solution submission for the perfect-numbers.jl exercise on Exercism.io Julia track: all tests passed

--- julia/perfect-numbers/perfect-numbers.jl
+++ julia/perfect-numbers/perfect-numbers.jl
@@ -1,26 +1,9 @@
 function sum_of_factors(number)
-	i = 1
-	sum_of_factors = 0
-	if number == 0 # number provide is equal to zero
-		throw(DomainError(number,"number is not natural"))
-	end
-	if number < 0 # number provided is negative
-		throw(DomainError(number, "number cannot be negative"))
-	end
-	while i <= number / 2 # smaller than half the number
-		if number % i == 0 # is a factor of the number
-			sum_of_factors += i # add to aliquot sum
-		end
-		i += 1 # increment
-	end
-	return sum_of_factors
+	(number > 0) || # test positivity
+	(number == 0 && throw(DomainError(number, "number cannot be zero")) || # number is zero
+	throw(DomainError(number, "number cannot be negative"))) # number provided is negative
+	number > 1 ? sum( x -> (number % x == 0 ? x : 0), 1:div(number,2)) : 0 # sum of aliquot
 end
-function isperfect(number) # number equal to its aliquot sum
-	return sum_of_factors(number) == number
-end
-function isabundant(number) # number smaller than its aliquot sum
-	return sum_of_factors(number) > number
-end
-function isdeficient(number) # number bigger than its aliquot sum
-	return sum_of_factors(number) < number
-end
+isperfect(number) = sum_of_factors(number) == number # number equal to its aliquot sum
+isabundant(number) = sum_of_factors(number) > number # number smaller than its aliquot sum
+isdeficient(number) = sum_of_factors(number) < number # number bigger than its aliquot sum
